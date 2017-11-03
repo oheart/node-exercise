bf58481445d7dd4ddebda26da0b2ba556af7082a
yuebanwan <2353769123@qq.com>
2017-11-03 17:06:58 +0800

    修改created_at方法

--- myblog/config/default.js
+++ myblog/config/default.js
@@ -13,4 +13,4 @@ module.exports = {
 //     2.session：express-session的配置信息
 //     3.mongodb: mongodb的地址，myblog为db名
 
-    // mongodb: 'mongodb://localhost:27017/myblog'
+
--- myblog/lib/mongo.js
+++ myblog/lib/mongo.js
@@ -10,13 +10,13 @@ var objectIdToTimestamp = require('objectid-to-timestamp');
 mongolass.plugin('addCreatedAt',{
     afterFind:function(results){
         results.forEach(function (item){
-            item.create_at = moment(objectIdToTimestamp(item._id)).format('YYYY-MM-DD HH:mm');
+            item.created_at = moment(objectIdToTimestamp(item._id)).format('YYYY-MM-DD HH:mm');
         });
         return results;
     },
     afterFindOne: function(result){
         if(result){
-            result.create_at = moment(objectIdToTimestamp(result._id)).format('YYYY-MM-DD HH:mm');
+            result.created_at = moment(objectIdToTimestamp(result._id)).format('YYYY-MM-DD HH:mm');
         }
         return result;
     }
